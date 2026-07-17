870a1f0b128df7be0df0d1687f8309afba36c3aa
Sirivalli Avutu <avutusirivalli.r@gmail.com>
2026-07-17 14:58:06 +0530

    after js and css

---
+++ src/test/java/com/SirivalliAvutu/AutoSuggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SirivalliAvutu.AutoSuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
