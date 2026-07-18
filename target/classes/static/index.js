1cd51191f6a8336a8502387bdf651875131fdfe3
Sirivalli Avutu <avutusirivalli.r@gmail.com>
2026-07-18 12:00:13 +0530

    after toggleuser

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,2 +1,39 @@
-// alert("hello hey")
-console.log("Hello from JS");
+alert("view profile")
+// console.log("Hello from JS");
+
+// data = {
+//     "name" : "John",
+//     "gender" : "Male"
+// }
+
+// data.name
+
+var users = [
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    //get elements
+
+    //1:image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+    //1:name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+    //1:gender
+    var userGender = document.getElementById("user-gender")
+    userGender.innerHTML = users[id].gender;
+}
