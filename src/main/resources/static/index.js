ab76ac3a7dfa8133f85968f03639881a75b668a3
Sirivalli Avutu <avutusirivalli.r@gmail.com>
2026-07-18 12:49:01 +0530

    after adding random user

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -34,6 +34,33 @@ function toggleUser(){
     var userName = document.getElementById("user-name");
     userName.innerHTML = users[id].name;
     //1:gender
-    var userGender = document.getElementById("user-gender")
+    var userGender = document.getElementById("user-gender");
     userGender.innerHTML = users[id].gender;
+}
+
+function randomUser(){
+    fetch('https://randomuser.me/api/')
+        .then(function(response){
+            //convert raw text to json
+            return response.json();
+        })
+        .then(function(data){
+            
+            var userData = data.results[0];
+
+            //1:image
+            var userImage = document.getElementById("user-image");
+            userImage.src = userData.picture.large;
+
+            //1:name
+            var userName = document.getElementById("user-name");
+            userName.innerHTML = userData.name.first + " " + userData.name.last;
+
+            //1:gender
+            var userGender = document.getElementById("user-gender");
+            userGender.innerHTML = userData.gender;
+        })
+        .catch(function(err){
+            console.log(err);
+        })
 }
